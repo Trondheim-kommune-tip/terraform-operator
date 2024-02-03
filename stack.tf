43e991993d40a2d54809827a3fb054222dba44d0
Alok Hom <alok.hom@trondheim.kommune.no>
2024-02-03 11:23:43 +0100

    TKCL-372 seect tf version 1.3.0

--- stack.tf
+++ stack.tf
@@ -11,6 +11,7 @@ resource "spacelift_stack" "managed" {
   project_root = "managed-stack"
   administrative = true
   autodeploy = false
+  terraform_version = "1.3.0"
   labels     = ["managed", "depends-on:${data.spacelift_current_stack.this.id}"]
 }
 
