28ebb094c707f86a701d27e2c29001abef0e33cf
Alok Hom <alok.hom@trondheim.kommune.no>
2024-02-03 09:33:09 +0100

    TKCL-372 strong password

--- managed-stack/main.tf
+++ managed-stack/main.tf
@@ -1,6 +1,7 @@
 # This resource here is to show you how plan policies work.
 
 resource "random_password" "secret" {
-  length  = 8
+  length  = 36
   special = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
 }
