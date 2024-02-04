1518aee15ae1f58261702c2fd41c318fbd14424f
Alok Hom <alok.hom@trondheim.kommune.no>
2024-02-04 18:28:28 +0100

    TKCL-376 arrange rbac ad a non module

--- managed-stack/development/avd-rbac/main.tf
+++ managed-stack/development/avd-rbac/main.tf
@@ -1,3 +1,8 @@
+# configure module
+module "avd-configure" {
+    source = "./modules/avd-configure"
+}
+
 data "azuread_user" "aad_user" {
   for_each            = toset(var.avd_users)
   user_principal_name = format("%s", each.key)
