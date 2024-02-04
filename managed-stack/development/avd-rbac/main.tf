8cfb7932894e2d7078dd3754a1895da4dfb1fd5e
Alok Hom <alok.hom@trondheim.kommune.no>
2024-02-04 17:32:06 +0100

    TKCL-376 moved spacelift provider to providers.tf

--- managed-stack/development/avd-rbac/main.tf
+++ managed-stack/development/avd-rbac/main.tf
@@ -1,11 +1,3 @@
-terraform {
-  required_providers {
-    spacelift = {
-      source = "spacelift-io/spacelift"
-    }
-  }
-}
-
 data "azuread_user" "aad_user" {
   for_each            = toset(var.avd_users)
   user_principal_name = format("%s", each.key)
--- managed-stack/development/avd-rbac/providers.tf
+++ managed-stack/development/avd-rbac/providers.tf
@@ -1,5 +1,8 @@
 terraform {
   required_providers {
+    spacelift = {
+      source = "spacelift-io/spacelift"
+    }
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~>3.0"
