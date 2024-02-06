84fb7dc143307a9f4c5fcf46960f18736afbc07c
Alok Hom <alok.hom@trondheim.kommune.no>
2024-02-06 15:46:42 +0100

    TKCL-381 change UPN to user_principal_name

--- managed-stack/development/avd-rbac/main.tf
+++ managed-stack/development/avd-rbac/main.tf
@@ -1,6 +1,6 @@
 data "azuread_user" "aad_user" {
   for_each            = toset(var.avd_users)
-  user_principal_names = format("%s", each.key)
+  user_principal_name = format("%s", each.key)
 }
 
 data "azurerm_role_definition" "role" { # access an existing built-in role
