b4ea955b2af6f3786c279e641cbfee8e70cde781
Alok Hom <alok.hom@trondheim.kommune.no>
2024-02-06 13:35:13 +0100

    TKCL-381 avd users email id

--- managed-stack/development/avd-rbac/variables.tf
+++ managed-stack/development/avd-rbac/variables.tf
@@ -1,10 +1,10 @@
 variable "avd_users" {
   description = "AVD users"
   default = [
-    "Mikal.Rekdal_avoconsulting.no#EXT#@trondheim.onmicrosoft.com",
-    "sarumathy.soosaipillai_avoconsulting.no#EXT#@trondheim.onmicrosoft.com",
-    "Sebastian.S.Eide_avoconsulting.no#EXT#@trondheim.onmicrosoft.com",
-    "josefine.jornsen_avoconsulting.no#EXT#@trondheim.onmicrosoft.com"
+    "mikal.rekdal@avoconsulting.no",
+    "sarumathy.soosaipillai@avoconsulting.no",
+    "sebastian.s.eide@avoconsulting.no",
+    "josefine.jornsen@avoconsulting.no"
   ]
 }
 
