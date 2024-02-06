b9892b542343f6986ef624b286343ffda37d9ea5
Alok Hom <alok.hom@trondheim.kommune.no>
2024-02-06 16:28:29 +0100

    TKCL-381 testing locals and upn

--- managed-stack/development/avd-rbac/main.tf
+++ managed-stack/development/avd-rbac/main.tf
@@ -1,6 +1,13 @@
+locals {
+  user_name = "mikal.rekdal@avoconsulting.no"
+  tenant_domain = "trondheim.onmicrosoft.com"
+  user_principal_name = "${replace(local.user_name, "@", "_")}#EXT#@${local.tenant_domain}"
+}
+
 data "azuread_user" "aad_user" {
-  for_each            = toset(var.avd_users)
-  user_principal_name = format("%s", each.key)
+  #for_each            = toset(var.avd_users)
+  user_principal_name = local.user_principal_name
+  #user_principal_name = format("%s", each.key)
 }
 
 data "azurerm_role_definition" "role" { # access an existing built-in role
