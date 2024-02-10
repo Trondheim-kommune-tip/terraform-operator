a25fa0ff666f7d69b0cf320e3405e8f3bc5cca16
Alok Hom <alok.hom@trondheim.kommune.no>
2024-02-10 19:21:00 +0100

    TKCL-383 add id defaults

--- managed-stack/development/avd-createhost-and-network/variables.tf
+++ managed-stack/development/avd-createhost-and-network/variables.tf
@@ -130,36 +130,43 @@ variable "azurerm_virtual_desktop_host_pool_registration_info_registrationinfo_t
 variable "arm_client_id" {
   type = string
   description = "azure AD client id"
+  default = "id"
 }
 
 variable "arm_client_secret" {
   type = string
   description = "azure AD client secret"
+  default = "id"
 }
 
 variable "arm_tenant_id" {
   type = string
   description = "azure AD tenant id"
+  default = "id"
 }
 
 variable "arm_subscription_id" {
   type = string
   description = "azure AD subs id"
+  default = "id"
 }
 
 variable "azurerm_virtual_desktop_application_group_dag_id" {
   type = string
   description = "azure AD azurerm_virtual_desktop_application_group_dag_id id"
+  default = "id"
 }
 
 variable "azurerm_virtual_desktop_workspace_workspace_id" {
   type = string
   description = "azure AD azurerm_virtual_desktop_workspace_workspace_id id"
+  default = "id"
 }
 
 variable "azure_virtual_desktop_host_pool_hostpool_id" {
   type = string
   description = "azure AD azure_virtual_desktop_host_pool_hostpool_id id"
+  default = "id"
 }
 
 variable "aad_group_name" {
