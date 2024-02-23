a16c7acdfd35784543f38668eda2d000358a15ba
Alok Hom <alok.hom@trondheim.kommune.no>
2024-02-23 14:08:05 +0100

    TKCL-383 paas services storage account  donot allow public access

--- managed-stack/development/avd-createhost-and-network/main.tf
+++ managed-stack/development/avd-createhost-and-network/main.tf
@@ -418,6 +418,7 @@ resource "azurerm_storage_account" "storage" {
   account_tier             = "Premium"
   account_replication_type = "LRS"
   account_kind             = "FileStorage"
+  public_network_access_enabled = false
 }
 
 resource "azurerm_storage_share" "FSShare" {
