33fb987ef331f19c228014df432076e9870b79b0
Alok Hom <alok.hom@trondheim.kommune.no>
2024-02-27 15:08:29 +0100

    TKCL-383 fix subnet as a string

--- managed-stack/development/avd-createhost-and-network/c_vnet_peering.tf
+++ managed-stack/development/avd-createhost-and-network/c_vnet_peering.tf
@@ -24,7 +24,7 @@ resource "azurerm_virtual_network" "vnet" {
 
   subnet {
     name           = "default"
-    address_prefix = "${var.subnet_range}"
+    address_prefix = var.subnet_range
     security_group = azurerm_network_security_group.nsg.id
   }
 
