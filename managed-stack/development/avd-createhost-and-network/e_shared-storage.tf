5a2e309a4feba2b36a8c88bd3d0c8a8d087b7d83
Alok Hom <alok.hom@trondheim.kommune.no>
2024-03-15 16:36:41 +0100

    TKCL-383 azure storage abridged scope

--- managed-stack/development/avd-createhost-and-network/e_shared-storage.tf
+++ managed-stack/development/avd-createhost-and-network/e_shared-storage.tf
@@ -1,54 +1,6 @@
 ########################
 #### shared storage 
-## Create a Resource Group for Storage
-#resource "azurerm_resource_group" "rg_storage" {
-#  location = var.deploy_location
-#  name     = var.rg_stor
-#}
 
-
-resource "azurerm_subnet" "subnet-storage" {
-  name                 = "default-storage"
-  resource_group_name  = "${var.azure_virtual_desktop_compute_resource_group}"
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = var.storage_subnet_range
-  service_endpoints    = ["Microsoft.Sql", "Microsoft.Storage", "Microsoft.AzureActiveDirectory"]
-  depends_on           = [azurerm_resource_group.rg]    #### rg-avd-compute
-}
-
-resource "azurerm_network_security_group" "nsg-storage" {
-  name                = "${var.prefix}-storage-NSG"
-  location            = var.deploy_location
-  resource_group_name = "${var.azure_virtual_desktop_compute_resource_group}"
-  security_rule {
-    name                       = "vm2smbstorage"
-    priority                   = 100
-    direction                  = "Outbound"
-    access                     = "Allow"
-    protocol                   = "*"
-    source_port_range          = "*"
-    destination_port_ranges    = ["445"]
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-  security_rule {
-    name                       = "smbstorageoutbound2internet"
-    priority                   = 110
-    direction                  = "Outbound"
-    access                     = "Deny"
-    protocol                   = "*"
-    source_port_range          = "*"
-    destination_port_range     = "*"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-  depends_on = [azurerm_resource_group.rg]               #### rg-avd-compute
-}
-
-resource "azurerm_subnet_network_security_group_association" "nsg_assoc_subnet_storage" {
-  subnet_id                 = azurerm_subnet.subnet-storage.id
-  network_security_group_id = azurerm_network_security_group.nsg-storage.id
-}
 
 # generate a random string (consisting of four characters)
 # https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
@@ -69,8 +21,6 @@ resource "azurerm_storage_account" "storage" {
   account_tier             = "Premium"
   account_replication_type = "LRS"
   account_kind             = "FileStorage"
-  allow_nested_items_to_be_public = false
-  public_network_access_enabled = false
 }
 
 resource "azurerm_storage_share" "FSShare" {
@@ -87,14 +37,6 @@ resource "azurerm_storage_share" "FSShare" {
       expiry      = "2024-12-23T10:38:21.0000000Z"
     }
   }
-}
-
-resource "azurerm_storage_account_network_rules" "storage" {
-  storage_account_id         = azurerm_storage_account.storage.id
-  ip_rules                  = ["127.0.0.1"]
-  virtual_network_subnet_ids = [azurerm_subnet.subnet-storage.id]
-  default_action             = "Allow"
-  bypass                     = ["AzureServices", "Logging", "Metrics"]
 }
 
 ## Azure built-in roles
