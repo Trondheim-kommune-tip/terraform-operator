12cd274444defd77dc5cad67a72d3182002d4bfa
Alok Hom <alok.hom@gmail.com>
2024-03-16 16:22:59 +0100

    TKCL-383 add mssql db

--- managed-stack/development/avd-createhost-and-network/c_session_hosts.tf
+++ managed-stack/development/avd-createhost-and-network/c_session_hosts.tf
@@ -46,7 +46,6 @@ resource "azurerm_network_interface" "avd_vm_nic" {
   name                = "${var.prefix}-${count.index + 1}-nic"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  edge_zone           = "1"
 
   ip_configuration {
     name                          = "nic${count.index + 1}_config"
