ce419c9409f5257a57b3b7a59ab3d9ccc8cb6ad6
Alok Hom <alok.hom@gmail.com>
2024-03-23 13:10:49 +0100

    TKCL-383 fix role def

--- managed-stack/development/avd-createhost-and-network/a_rbac.tf
+++ managed-stack/development/avd-createhost-and-network/a_rbac.tf
@@ -22,63 +22,63 @@ data "azurerm_role_definition" "role_viewonportal_nd_login" {
 }
 
 # resource "azuread_group" "aad_group" was earlier
-#data "azuread_group" "aad_group" {
-#  display_name     = var.aad_group_name
+data "azuread_group" "aad_group" {
+  display_name     = var.aad_group_name
+  security_enabled = true
+}
+
+#resource "azuread_group" "aad_group" {
+#  display_name     = var.aad_group_name_avd
 #  security_enabled = true
+#  provider = azuread.rpa
 #}
 
-resource "azuread_group" "aad_group" {
-  display_name     = var.aad_group_name_avd
-  security_enabled = true
-  provider = azuread.rpa
-}
-
-resource "azuread_user" "aad_user" {
-  for_each            = toset(var.avd_users)
-  display_name        = format("%s", each.key)
-  user_principal_name = format("%s", each.key)
-  password            = "Avdaccess123@"
-  force_password_change = true
-}
+#resource "azuread_user" "aad_user" {
+#  for_each            = toset(var.avd_users)
+#  display_name        = format("%s", each.key)
+#  user_principal_name = format("%s", each.key)
+#  password            = "Avdaccess123@"
+#  force_password_change = true
+#}
 
 #data "azuread_user" "robot" {
 #  user_principal_name = "robot.roger.robot.roger@trondheim.kommune.no"
 #}
 
-resource "azuread_group_member" "aad_group_member" {
-  for_each         = azuread_user.aad_user
-  group_object_id  = azuread_group.aad_group.id
-  member_object_id = each.value["id"]
-}
+#resource "azuread_group_member" "aad_group_member" {
+#  for_each         = azuread_user.aad_user
+#  group_object_id  = data.azuread_group.aad_group.id
+#  member_object_id = each.value["id"]
+#}
 
-resource "azurerm_role_assignment" "role_useraccount" {
-  for_each           = azuread_user.aad_user
-  scope              = azuread_user.aad_user[each.key].object_id
-  role_definition_id = data.azurerm_role_definition.role_viewonportal_nd_login.id
-  principal_id       = azuread_group.aad_group.object_id
-}
+#resource "azurerm_role_assignment" "role_useraccount" {
+#  for_each           = azuread_user.aad_user
+#  scope              = azuread_user.aad_user[each.key].object_id
+#  role_definition_id = data.azurerm_role_definition.role_viewonportal_nd_login.id
+#  principal_id       = data.azuread_group.aad_group.object_id
+#}
 
 resource "azurerm_role_assignment" "role_dag" {
   scope              = "${var.azurerm_virtual_desktop_application_group_dag_id}"
   role_definition_id = data.azurerm_role_definition.role_dvc.id
-  principal_id       = azuread_group.aad_group.object_id
+  principal_id       = data.azuread_group.aad_group.object_id
 }
 
 resource "azurerm_role_assignment" "role_workspace" {
   scope              = "${var.azurerm_virtual_desktop_workspace_workspace_id}"
   role_definition_id = data.azurerm_role_definition.role_dvc.id
-  principal_id       = azuread_group.aad_group.object_id
+  principal_id       = data.azuread_group.aad_group.object_id
 }
 
 resource "azurerm_role_assignment" "role_hostpool" {
   scope              = "${var.azure_virtual_desktop_host_pool_hostpool_id}"
   role_definition_id = data.azurerm_role_definition.role_dvc.id
-  principal_id       = azuread_group.aad_group.object_id
+  principal_id       = data.azuread_group.aad_group.object_id
 }
 
 resource "azurerm_role_assignment" "role_sessionhost" {
   count              = var.rdsh_count
   scope              = azurerm_windows_virtual_machine.avd_vm.*.id[count.index]
   role_definition_id = data.azurerm_role_definition.role_session_host.id
-  principal_id       = azuread_group.aad_group.object_id
+  principal_id       = data.azuread_group.aad_group.object_id
 }
--- managed-stack/development/avd-createhost-and-network/output.tf
+++ managed-stack/development/avd-createhost-and-network/output.tf
@@ -36,7 +36,7 @@ output "subsid" {
 
 output "AVD_user_groupname" {
   description = "Azure Active Directory Group for AVD users"
-  value       = azuread_group.aad_group.display_name
+  value       = data.azuread_group.aad_group.display_name
 }
 
 output "storage_account" {
