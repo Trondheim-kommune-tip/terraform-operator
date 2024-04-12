23eccb0caacb44ff8b1c975452914d4d49e7aac6
Alok Hom <alok.hom@trondheim.kommune.no>
2024-04-12 14:03:18 +0200

    TKCL-383 reverting from 10.103.128.0/17

--- managed-stack/development/avd-createhost-and-network/b_vnet_peering.tf
+++ managed-stack/development/avd-createhost-and-network/b_vnet_peering.tf
@@ -23,7 +23,7 @@ resource "azurerm_virtual_network" "vnet" {
   resource_group_name = "${var.azure_virtual_desktop_compute_resource_group}"           #### rg-avd-resources 
   subnet {
     name           = "default"
-    address_prefix = "10.103.128.0/17"
+    address_prefix = "10.1.1.0/24"
     security_group = azurerm_network_security_group.nsg.id
   }
   tags = {
@@ -55,8 +55,8 @@ resource "azurerm_network_security_group" "nsg" {
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_ranges     = ["8181","10000","1001-1002"]
-    source_address_prefix      = "10.103.128.0/17"
-    destination_address_prefix = "10.103.128.0/17"
+    source_address_prefix      = "10.1.1.0/24"
+    destination_address_prefix = "10.1.1.0/24"
   }
   security_rule {
     name                       = "clients2appserver"
@@ -66,8 +66,8 @@ resource "azurerm_network_security_group" "nsg" {
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_ranges     = ["8196-8198","443","10000-10002","135","49152-65535"]
-    source_address_prefix      = "10.103.128.0/17"
-    destination_address_prefix = "10.103.128.0/17"
+    source_address_prefix      = "10.1.1.0/24"
+    destination_address_prefix = "10.1.1.0/24"
   }
   security_rule {
     name                       = "appserver2sqlnortheurope"
@@ -77,7 +77,7 @@ resource "azurerm_network_security_group" "nsg" {
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_ranges    = ["11000-11999"]
-    source_address_prefix      = "10.103.128.0/17"
+    source_address_prefix      = "10.1.1.0/24"
     destination_address_prefixes = ["13.69.224.0/26","13.69.224.192/26","13.69.225.0/26","13.69.225.192/26","13.69.233.136/29","13.69.239.128/26","13.74.104.64/26","13.74.104.128/26","13.74.105.0/26","13.74.105.128/26","13.74.105.192/29","20.50.73.32/27","20.50.73.64/26","20.50.81.0/26","20.166.43.0/25","20.166.45.0/24","23.102.16.130/32","23.102.52.155/32","40.85.102.50/32","40.113.14.53/32","40.113.16.190/32","40.113.93.91/32","40.127.128.10/32","40.127.137.209/32","40.127.141.194/32","40.127.177.139/32","52.138.224.0/26","52.138.224.128/26","52.138.225.0/26","52.138.225.128/26","52.138.229.72/29","52.146.133.128/25","65.52.225.245/32","65.52.226.209/32","68.219.193.128/25","104.41.202.30/32","191.235.193.75/32","191.235.193.139/32","191.235.193.140/31"]
   }
   security_rule {
@@ -88,7 +88,7 @@ resource "azurerm_network_security_group" "nsg" {
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_ranges    = ["1433-1434"]
-    source_address_prefix      = "10.103.128.0/17"
+    source_address_prefix      = "10.1.1.0/24"
     destination_address_prefixes = ["52.138.224.1/32","13.74.104.113/32"]
   }
   security_rule {
@@ -110,7 +110,7 @@ resource "azurerm_network_security_group" "nsg" {
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_ranges    = ["88","135","139","389","445","53","389","49152-65535"]
-    source_address_prefix      = "10.103.128.0/17"
+    source_address_prefix      = "10.1.1.0/24"
     destination_address_prefixes = ["10.68.5.0/24","10.68.0.0/24"]
   }
   security_rule {
@@ -121,7 +121,7 @@ resource "azurerm_network_security_group" "nsg" {
     protocol                   = "Udp"
     source_port_range          = "*"
     destination_port_ranges    = ["53","389"]
-    source_address_prefix      = "10.103.128.0/17"
+    source_address_prefix      = "10.1.1.0/24"
     destination_address_prefixes = ["10.68.5.0/24","10.68.0.0/24"]
   }
   depends_on = [azurerm_resource_group.rg]               #### rg-avd-compute
