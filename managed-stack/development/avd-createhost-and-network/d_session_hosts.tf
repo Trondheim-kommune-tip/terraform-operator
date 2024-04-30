3a76f4abd9874eb69a6c18e07a1d6ae43f127435
Alok Hom <alok.hom@trondheim.kommune.no>
2024-04-30 20:39:01 +0200

    TKCL-383 move vms and db to norwayeast

--- managed-stack/development/avd-createhost-and-network/d_session_hosts.tf
+++ managed-stack/development/avd-createhost-and-network/d_session_hosts.tf
@@ -33,7 +33,7 @@ resource "azurerm_public_ip" "avd_ext_ip" {
   name                    = "avd-ip-${count.index + 1}"
   location                = azurerm_resource_group.rg.location           # norwayeast
   resource_group_name     = azurerm_resource_group.rg.name
-  allocation_method       = "Static"
+  allocation_method       = "Dynamic"
   idle_timeout_in_minutes = 30
 
   tags = {
